003c8bf2f3c9d5615964a23264d26bce2143b0ba
hadiazb <hadiazb@unal.edu.co>
2020-11-12 18:36:32 -0500

    [ADD] card profile

--- src/Containers/Profile.js
+++ src/Containers/Profile.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import FormProfile from '../Components/FormProfile';
+import { HiPhotograph } from 'react-icons/hi';
+import { MdPhotoCamera } from 'react-icons/md';
 import '../Styles/Containers/Profile.css';
 
-const Profile = () => {
+const Profile = (props) => {
+	const url =
+		'https://firebasestorage.googleapis.com/v0/b/portafolio-a8d13.appspot.com/o/Potafolio%2FBackground%2FbackgroundCard.jpg?alt=media&token=3f10df12-defc-49c9-8b46-341f14a4d3e4';
+
+	const photo =
+		'https://firebasestorage.googleapis.com/v0/b/portafolio-a8d13.appspot.com/o/Potafolio%2FProfile%2FFotosHugo%2Fprofile.jpg?alt=media&token=7752a102-74bd-42d1-96d3-1445c426e499';
+
 	return (
 		<div className='profile'>
 			<div className='profile__update'>
@@ -14,8 +22,28 @@ const Profile = () => {
 			<div className='profile__card'>
 				<div
 					className='profile__card-header'
-					style={{ backgroundColor: '#000000' }}
-				></div>
+					style={{
+						backgroundImage: `url(${url})`,
+					}}
+				>
+					<img
+						src={props.photo ? props.photo : photo}
+						alt='foto'
+						className='photo'
+					/>
+				</div>
+				<div className='profile__card-header-input'>
+					<div className='file'>
+						<input type='file' className='file__input' />
+						<HiPhotograph size='20' />
+					</div>
+				</div>
+				<div className='profile__card-header-photo'>
+					<div className='file'>
+						<input type='file' className='file__input' />
+						<MdPhotoCamera size='50' />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
